21cdba097d6c9e65f04d1e0ab0d0b1988760446a
Klp71195 <klp71195@gmail.com>
2025-06-27 15:26:36 -0600

    Subida limpia ignorando archivos temporales

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AMOR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
